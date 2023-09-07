30d96d4351a503ba1d1e13658e926b3093e857df
Diego David <diegozelao@gmail.com>
2023-09-07 05:43:48 -0300

    feat(prisma): add prisma

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,9 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
+import { User } from '@prisma/client';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prisma: PrismaService,
+  ) {}
+
+  @Get('user')
+  async getAllUser(): Promise<User[]> {
+    return this.prisma.user.findMany();
+  }
 
   @Get()
   getHello(): string {
